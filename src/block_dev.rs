137a0b144e3b9f4f83f29e4fe386df72b368d7d6
Michael Daffin <michael@daffin.io>
2018-05-28 10:01:26 +0100

    Moves the checks into BlockDev Impl

--- src/block_dev.rs
+++ src/block_dev.rs
@@ -67,7 +67,7 @@ impl BlockDevice {
             flags: Vec::new(),
         };
 
-        blkdev.flags = check_all(&blkdev)?;
+        blkdev.check_none_removable()?;
         Ok(blkdev)
     }
 
@@ -93,6 +93,34 @@ impl BlockDevice {
 
     pub fn size(&self) -> Size {
         self.size
+    }
+
+    fn check_none_removable(&mut self) -> Result<(), io::Error> {
+        if if_exists!(read_to_string(self.sys_path().join("removable")))?
+            .map(|val| val.trim() == "0")
+            .unwrap_or(false)
+        {
+            self.flags.push(Reason::NonRemovable);
+        }
+        Ok(())
+    }
+
+    fn is_mounted(&mut self) -> Result<(), io::Error> {
+        if fs::read_to_string("/proc/mounts")?
+        .lines()
+        .map(|line| line.split_whitespace().next_tuple())
+        .filter_map(|line| line) // Filter out blank lines
+        .any(|(dev, _)| {
+            let dev_name = self.dev_file();
+            let dev_name = dev_name.to_str().expect("none unicode char in device path");
+            let part_name = PathBuf::from(dev);
+            let part_name = part_name.to_str().expect("none unicode char in mount file");
+
+            part_name.starts_with(dev_name)
+        }) {
+            self.flags.push(Reason::Mounted);
+        }
+        Ok(())
     }
 }
 
@@ -171,46 +199,4 @@ impl fmt::Display for Reason {
             }
         )
     }
-}
-
-pub fn check_all(blkdev: &BlockDevice) -> Result<Vec<Reason>, io::Error> {
-    let mut reasons = Vec::new();
-
-    if is_mounted(blkdev)? {
-        reasons.push(Reason::Mounted)
-    }
-
-    if is_none_removable(blkdev)? {
-        reasons.push(Reason::NonRemovable)
-    }
-
-    Ok(reasons)
-}
-
-// Logic used to see if the given device is considered one the is safe to write to. There is a
-// varity of crtera that will be considered for this flag. Currently it is just the removable
-// flag which is not enough as some devices that are safe are marked as none removable while
-// others that are no are marked. This function will deal with any corner cases that pop up.
-fn is_none_removable(blkdev: &BlockDevice) -> Result<bool, io::Error> {
-    Ok(
-        if_exists!(read_to_string(blkdev.sys_path().join("removable")))?
-            .map(|val| val.trim() == "0")
-            .unwrap_or(false),
-    )
-}
-
-fn is_mounted(blkdev: &BlockDevice) -> Result<bool, io::Error> {
-    let mounts = fs::read_to_string("/proc/mounts")?;
-    Ok(mounts
-        .lines()
-        .map(|line| line.split_whitespace().next_tuple())
-        .filter_map(|line| line) // Filter out blank lines
-        .any(|(dev, _)| {
-            let dev_name = blkdev.dev_file();
-            let dev_name = dev_name.to_str().expect("none unicode char in device path");
-            let part_name = PathBuf::from(dev);
-            let part_name = part_name.to_str().expect("none unicode char in mount file");
-
-            part_name.starts_with(dev_name)
-        }))
 }
